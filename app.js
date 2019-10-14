f10a2ea878a6a3d727a1517ebc12dea53892651a
Maurizio De Marchis <mauriziodm91@gmail.com>
2019-10-13 21:12:51 -0300

    middleware added

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 
 const port = 5000;
 
-app.listen(port, ()=>{
+//middleware section
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//ROUTES
+
+app.get('/', function (req, res){
+    res.send('INDEX');
+});
+
+app.get('/about', function (req, res){
+    app.send("about");
+});
+
+app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
