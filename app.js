bbfb9c5dc1c70522091ce2d0116d9173f07ae54e
Maurizio De Marchis <mauriziodm91@gmail.com>
2019-10-23 03:54:04 -0300

    navbar modified and add idea route created

--- app.js
+++ app.js
@@ -11,31 +11,43 @@ mongoose.Promise = global.Promise;
 
 //connect to mongoose
 
-mongoose.connect('mongodb://localhost/vidjot-dev', {
-    useMongoClient : true
-})
-.then(() => console.log('MongoDB Connected...'))
-.catch(err =>console.log(err));
+mongoose
+  .connect('mongodb://localhost/vidjot-dev', {
+    useMongoClient: true
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+//load idea model
+
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 //middleware section
 
-app.engine('handlebars', exphbs({
+app.engine(
+  'handlebars',
+  exphbs({
     defaultLayout: 'main'
-}));
+  })
+);
 app.set('view engine', 'handlebars');
 
 //ROUTES
 
-app.get('/', function (req, res){
-    const title = 'Welcome';
-    res.render('index', {title : title
-    });
+app.get('/', function(req, res) {
+  const title = 'Welcome';
+  res.render('index', { title: title });
 });
 
-app.get('/about', function (req, res){
-    res.render('about');
+app.get('/about', function(req, res) {
+  res.render('about');
+});
+
+app.get('/ideas/add', function(req, res) {
+  res.render('ideas/add');
 });
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
